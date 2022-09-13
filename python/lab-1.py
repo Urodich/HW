c4d9c1b1c0a1419b054e66c2b869e536cb675884
Urodich <maks.zhuravscky@mail.ru>
2022-09-13 19:47:45 +0500

    Update lab-1.py

--- python/lab-1.py
+++ python/lab-1.py
@@ -2,8 +2,8 @@ import collections
 from queue import Queue
 import queue
 from PIL import Image
-import numpy
-import vector
+import numpy as np
+
 
 loop=False
 angle=0
@@ -11,48 +11,75 @@ max_angle=0
 
 q=Queue()
 
-
-pixx = numpy.asarray(Image.open('./картинки/1.bmp').convert('L'))
+pixx = np.asarray(Image.open('./картинки/1.bmp').convert('L'))
 pix=pixx.copy()
 
-line_vec = vector.obj(x=0,y=0)
+line_vec = np.array([0,0])
 
-A=[[vector.obj(x=0,y=0) for i in range(30)]for i in range(30)]
+A=[[np.array([0,0]) for i in range(30)]for i in range(30)]
 
 
 def Step(vect, tail):
-    i=tail[0][0]
-    j=tail[0][1]
+    i=tail[-1][0]
+    j=tail[-1][1]
+
     if i<0 or j<0 or i>29 or j>29: return
     if pix[i][j]!=0: return
+
     pix[i][j]=-1
     q.put([vect,tail])
     if tail.__len__==5: tail.pop(0)
-    Step( vector.obj(x=-1, y=-1),tail+[i-1, j-1])
-    Step( vector.obj(x=-1, y=0),tail+[i-1, j])
-    Step( vector.obj(x=-1, y=1),tail+[i-1, j+1])
-    Step( vector.obj(x=0, y=-1),tail+[i, j-1])
-    Step( vector.obj(x=0, y=1),tail+[i, j+1])
-    Step( vector.obj(x=1, y=-1),tail+[i+1, j-1])
-    Step( vector.obj(x=1, y=0),tail+[i+1, j])
-    Step( vector.obj(x=1, y=1),tail+[i+1, j+1])
+
+    tail.append([i-1, j-1])
+    Step( np.array([-1, -1]),tail)
+    tail.pop()
+    tail.append([i-1, j])
+    Step( np.array([-1, 0]),tail)
+    tail.pop()
+    tail.append([i-1, j+1])
+    Step( np.array([-1, 1]),tail)
+    tail.pop()
+    tail.append([i, j-1])
+    Step( np.array([0, -1]),tail)
+    tail.pop()
+    tail.append([i, j+1])
+    Step( np.array([0, 1]),tail)
+    tail.pop()
+    tail.append([i+1, j-1])
+    Step( np.array([1, -1]),tail)
+    tail.pop()
+    tail.append([i+1, j])
+    Step( np.array([1, 0]),tail)
+    tail.pop()
+    tail.append([i+1, j+1])
+    Step( np.array([1, 1]),tail)
+    tail.pop()
 
     return
 
 def wind(vect, track):
-    cur_vec = vector.obj(x=0,y=0)
+    cur_vec = np.array([0,0])
     for i in track:
-        cur_vec+=vect
-    A[track[0][0]][track[0][1]]=vector.normalize
+        cur_vec=cur_vec+(A[i[0]][i[1]])
+    cur_vec=cur_vec+(vect)
+    
+    print(np.linalg.norm(cur_vec))
+    A[track[-1][0]][track[-1][1]]=cur_vec
     return
-
 
 
 for i in range(30):
     for j in range(30):
         if pix[i][j]==0:
-            Step(vector.obj(x=0, y=0), [[i,j]])
+            Step(np.array([0, 0]), [[i,j]])
+            break
 
-for i in q:
+print(q.qsize())
+
+while not q.empty():
+    i=q.get()
     wind(i[0],i[1])
 
+
+for i in A:
+    print(i)
