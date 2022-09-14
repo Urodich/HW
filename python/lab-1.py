2d510dd68b14f3eecf2961c749355e19e0d8a57e
Urodich <76094917+Urodich@users.noreply.github.com>
2022-09-15 00:50:31 +0500

    Update lab-1.py

--- python/lab-1.py
+++ python/lab-1.py
@@ -1,9 +1,9 @@
 import collections
+from email.mime import image
 from queue import Queue
 import queue
 from PIL import Image
 import numpy as np
-
 
 loop=False
 angle=0
@@ -14,10 +14,10 @@ q=Queue()
 pixx = np.asarray(Image.open('./картинки/1.bmp').convert('L'))
 pix=pixx.copy()
 
-line_vec = np.array([0,0])
 
-A=[[np.array([0,0]) for i in range(30)]for i in range(30)]
+A=[[np.array([0,0,0]) for i in range(30)]for i in range(30)]
 
+start=[-1,-1]
 
 def Step(vect, tail):
     i=tail[-1][0]
@@ -26,60 +26,76 @@ def Step(vect, tail):
     if i<0 or j<0 or i>29 or j>29: return
     if pix[i][j]!=0: return
 
+    if start==[-1,-1]: start=[i,j]
+    elif start==[i,j]: loop=True
+
     pix[i][j]=-1
     q.put([vect,tail])
     if tail.__len__==5: tail.pop(0)
 
+
     tail.append([i-1, j-1])
-    Step( np.array([-1, -1]),tail)
+    Step( np.array([-1, -1,0]),tail)
     tail.pop()
     tail.append([i-1, j])
-    Step( np.array([-1, 0]),tail)
+    Step( np.array([-1, 0,0]),tail)
     tail.pop()
     tail.append([i-1, j+1])
-    Step( np.array([-1, 1]),tail)
+    Step( np.array([-1, 1,0]),tail)
     tail.pop()
     tail.append([i, j-1])
-    Step( np.array([0, -1]),tail)
+    Step( np.array([0, -1,0]),tail)
     tail.pop()
     tail.append([i, j+1])
-    Step( np.array([0, 1]),tail)
+    Step( np.array([0, 1,0]),tail)
     tail.pop()
     tail.append([i+1, j-1])
-    Step( np.array([1, -1]),tail)
+    Step( np.array([1, -1,0]),tail)
     tail.pop()
     tail.append([i+1, j])
-    Step( np.array([1, 0]),tail)
+    Step( np.array([1, 0,0]),tail)
     tail.pop()
     tail.append([i+1, j+1])
-    Step( np.array([1, 1]),tail)
+    Step( np.array([1, 1,0]),tail)
     tail.pop()
 
     return
 
 def wind(vect, track):
-    cur_vec = np.array([0,0])
+    cur_vec = np.array([0,0,0])
     for i in track:
         cur_vec=cur_vec+(A[i[0]][i[1]])
-    cur_vec=cur_vec+(vect)
+    cur_vec=cur_vec+vect
+
     
-    print(np.linalg.norm(cur_vec))
+    if np.linalg.norm(cur_vec)!=0:
+        cur_vec=cur_vec/np.linalg.norm(cur_vec)
     A[track[-1][0]][track[-1][1]]=cur_vec
     return
+
 
 
 for i in range(30):
     for j in range(30):
         if pix[i][j]==0:
-            Step(np.array([0, 0]), [[i,j]])
+            start=[i,j]
+            Step(np.array([0, 0,0]), [[i,j]])
             break
 
-print(q.qsize())
 
 while not q.empty():
     i=q.get()
     wind(i[0],i[1])
 
+def view():
+    red = [[i[0]*255 for i in j] for j in A]
+    green = [[i[1]*255 for i in j] for j in A]
+    blue = [[i[2]*255 for i in j] for j in A]
+    
 
-for i in A:
-    print(i)
+    out_data = np.stack((red, green, blue), axis=2).astype('uint8')
+    out_img = Image.fromarray(out_data)
+    out_img.show()
+
+view()
+print("loop",loop)
