a733d432f68617fdd23036926da35c9f8e580ac4
Urodich <76094917+Urodich@users.noreply.github.com>
2022-09-27 10:22:07 +0500

    OpenGl

---
+++ OpenGL/OpenGL/Lab1.cpp
@@ -0,0 +1,57 @@
+#include <GLFW/glfw3.h>
+
+int Width = 640;
+int Height = 480;
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+static void change_size_callback(GLFWwindow* window, int width, int height) {
+    Width = width;
+    Height = height;
+}
+
+int main1(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(Width, Height, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetWindowSizeCallback(window, change_size_callback);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+
+    }
+
+    glfwTerminate();
+    
+    return 0;
+}
+
