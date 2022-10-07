d23f602e8937690fc987bfd404782088030bc97f
Urodich <maks.zhuravscky@mail.ru>
2022-10-07 19:23:58 +0500

    gl3

--- OpenGL/OpenGL/Lab3.cpp
+++ OpenGL/OpenGL/Lab3.cpp
@@ -2,7 +2,7 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-#include "../linmath.h"
+#include "./linmath.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,7 +11,7 @@ static const struct
 {
     float x, y, z;
     float r, g, b;
-} squad[8] =
+} ssquad[8] =
 {
     { -0.2f, -0.2f, 0, 1.f, 1.f, 0.f },
     {  0.2f, -0.2f, 0, 0.f, 1.f, 1.f },
@@ -23,20 +23,62 @@ static const struct
     {  0.2f, 0.2f, 1, 1.f, 0.f, 0.f },
     { -0.2f, 0.2f, 1, 0.f, 1.f, 1.f }
 };
+GLfloat squad[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
 
 static const char* vertex_shader_text =
 "#version 110\n"
 "uniform mat4 MVP;\n"
-"uniform mat4 projectoin;\n"
+"uniform mat4 projection;\n"
 "uniform mat4 view;\n"
-"attribute vec3 vCol;\n"
+"attribute vec2 vCol;\n"
 "attribute vec3 vPos;\n"
 "varying vec3 color;\n"
 "void main()\n"
 "{\n"
-"    gl_Position = projection * view*MVP * vec4(vPos, 1.0);\n"
-"    color = vCol;\n"
+"    gl_Position = projection * view * MVP * vec4(vPos, 1.0f);\n"
+"    color = vec3(vCol, 0);\n"
 "}\n";
 
 
@@ -99,7 +141,7 @@ int main(void)
 {
     GLFWwindow* window;
     GLuint squad_vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
+    GLint mvp_location, projection_location, view_location, vpos_location, vcol_location;
 
     glfwSetErrorCallback(error_callback);
 
@@ -107,8 +149,13 @@ int main(void)
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    /*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);*/
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
     if (!window)
@@ -145,7 +192,7 @@ int main(void)
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
-    mvp_location = glGetUniformLocation(program, "MVP");
+    //mvp_location = glGetUniformLocation(program, "MVP");
     vpos_location = glGetAttribLocation(program, "vPos");
     vcol_location = glGetAttribLocation(program, "vCol");
 
@@ -179,7 +226,8 @@ int main(void)
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float)height;
 
-        mat4x4_ortho(m, -1.f, 1.f, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_identity(m);
+        //mat4x4_ortho(m, -1.f, 1.f, -1.f, 1.f, 1.f, -1.f);
 
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float)height;
@@ -194,20 +242,27 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         mvp_location = glGetUniformLocation(program, "MVP");
-        mvp_location = glGetUniformLocation(program, "view");
-        mvp_location = glGetUniformLocation(program, "projection");
+        view_location = glGetUniformLocation(program, "view");
+        projection_location = glGetUniformLocation(program, "projection");
 
         glUseProgram(program);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)m);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)look_at);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)projection);
+        glUniformMatrix4fv(view_location, 1, GL_FALSE, (const GLfloat*)look_at);
+        glUniformMatrix4fv(projection_location, 1, GL_FALSE, (const GLfloat*)projection);
         
          
         glBindBuffer(GL_ARRAY_BUFFER, squad_vertex_buffer);
-        glEnableVertexAttribArray(vpos_location);
+        /*glEnableVertexAttribArray(vpos_location);
         glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE, sizeof(squad[0]), (void*)0);
         glEnableVertexAttribArray(vcol_location);
-        glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(squad[0]), (void*)(sizeof(float) * 3));
+        glVertexAttribPointer(vcol_location, 2, GL_FLOAT, GL_FALSE, sizeof(squad[0]), (void*)(sizeof(float) * 3));*/
+
+        // Position attribute
+        glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+        glEnableVertexAttribArray(vpos_location);
+        // TexCoord attribute
+        glVertexAttribPointer(vcol_location, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(vcol_location);
 
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glDisableVertexAttribArray(vpos_location);
