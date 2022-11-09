c2d4f801da53e2dcb75e47bb1d85bf69b24a050f
Urodich <76094917+Urodich@users.noreply.github.com>
2022-11-09 16:55:31 +0500

    isit4

---
+++ python/lab4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Director
+    {
+        public void CreateProgram()
+        {
+
+        }
+    }
+    class Teacher
+    {
+           
+    }
+    class Student
+    {
+        public int Group;
+        public string Name;
+    }
+    class Shcool
+    {
+        Dictionary<int, Schedule> program;
+        List<Room> Rooms;
+    }
+    class Room
+    {
+        Student[,] tables;
+    }
+    struct Schedule
+    {
+
+    }
+}
